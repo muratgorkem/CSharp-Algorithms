8d19bb57e58fe51c17d00b214c1241ee7a6b663d
Murat Görkem <muratgorkem60@gmail.com>
2026-02-28 20:32:18 +0300

    ilk c# komutunu ekledim

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿
+using System;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.IO.Pipelines;
+using System.Reflection.Metadata;
+using Microsoft.VisualBasic;
+
+class Program
+{
+    //Write a C# Sharp program to create a string
+    // made of every other character starting with 
+    // the first in a given string.
+    static void Main(string[] args)
+    {
+       Console.WriteLine(Tata("Python"));
+       Console.WriteLine(Tata("PHP"));
+       Console.WriteLine(Tata("JS"));
+       
+    }
+
+     static public string Tata(string str)
+    {
+        string result="";
+
+        for(int i=0; i<str.Length; i++)
+        {
+           if (i % 2 == 0)
+            {
+              result += str[i]; 
+            }
+        }
+            return result;
+    
+    }}
+
+
+
+    
---
+++ ders1.cs
@@ -0,0 +1,19 @@
+// using System;
+// using System.Diagnostics.Contracts;
+// using System.Security.Cryptography.X509Certificates;
+
+// class Ders1
+// {
+//     public static void Yaz()
+//     {
+//         Console.WriteLine("Merhaba Murat");
+//     }
+// }
+
+// class Ders2
+// {
+//     public  static  int Topla(int x, int y)
+//     {
+//       return x + y; 
+//     }
+// }
